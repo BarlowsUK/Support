a48ca65f9a85f69fdd4a687a87decb0f25288e85
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-16 10:36:59 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,24 +24,3 @@ document.querySelectorAll('.filter-button').forEach(button => {
         });
     });
 });
-document.querySelectorAll('.filter-button').forEach(button => {
-    button.addEventListener('click', function() {
-        // Remove "active" class from all filter buttons
-        document.querySelectorAll('.filter-button').forEach(btn => {
-            btn.classList.remove('active');
-        });
-        // Add "active" class to the clicked button
-        this.classList.add('active');
-
-        let filterValue = this.getAttribute('data-filter').toLowerCase(); // Convert to lowercase
-        let x = document.querySelectorAll(".image_container");
-        x.forEach((item) => {
-            let id = item.id.toLowerCase(); // Get the id attribute
-            if (id.includes(filterValue)) { // Check if id contains the filter value
-                item.classList.remove("hidden");
-            } else {
-                item.classList.add("hidden");
-            }
-        });
-    });
-});
