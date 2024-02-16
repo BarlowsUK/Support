84bd511d7369ce495726147aa4eef0e56323988f
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-16 10:12:52 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,17 @@
       }
     });
   }
+document.querySelectorAll('.filter-button').forEach(button => {
+    button.addEventListener('click', function() {
+        let filterValue = this.getAttribute('data-filter');
+        let x = document.querySelectorAll(".image_container");
+        x.forEach((item) => {
+            let id = item.id.toLowerCase(); // Get the id attribute
+            if (id !== filterValue.toLowerCase()) {
+                item.classList.add("hidden");
+            } else {
+                item.classList.remove("hidden");
+            }
+        });
+    });
+});
