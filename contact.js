25beafb108eda9e00b1fb187c2e4d220f4c458f4
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-16 16:48:10 +0000

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,32 @@
-
+function filterLP(evt) {
+    let input = evt.target.value.toLowerCase();
+    let x = document.querySelectorAll(".image_container");
+    x.forEach((item) => {
+      let id = item.id.toLowerCase(); // Get the id attribute
+      if (!id.includes(input)) {
+        item.classList.add("hidden");
+      } else {
+        item.classList.remove("hidden");
+      }
+    });
+  }
+document.querySelectorAll('.filter-button').forEach(button => {
+    button.addEventListener('click', function() {
+        let filterValue = this.getAttribute('data-filter').toLowerCase(); // Convert to lowercase
+        let x = document.querySelectorAll(".image_container");
+        x.forEach((item) => {
+            let id = item.id.toLowerCase(); // Get the id attribute
+            if (id.includes(filterValue)) { // Check if id contains the filter value
+                item.classList.remove("hidden");
+            } else {
+                item.classList.add("hidden");
+            }
+        });
+    });
+});
+document.getElementById('clear-filters-button').addEventListener('click', function() {
+    let x = document.querySelectorAll(".image_container");
+    x.forEach((item) => {
+        item.classList.remove("hidden");
+    });
+});
