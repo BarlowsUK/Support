836b08b1a9ae1dce7843d866f70686c6a3d06eed
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-21 15:11:38 +0000

    Update passcode.js

--- js/passcode.js
+++ js/passcode.js
@@ -1,35 +1,23 @@
-// passcode.js
-function passcodeprotect() {
-    // Check if the passcode flag is set in localStorage
-    if (localStorage.getItem('passcodeEntered') === 'true') {
-        return; // If passcode already entered, exit the function
+    var password = "Barlowsfeb";
+
+    function passcodeprotect() {
+        // Check if the passcode flag is set in localStorage
+        if (localStorage.getItem('passcodeEntered') === 'true') {
+            return; // If passcode already entered, exit the function
+        }
+
+        var passcode = prompt("Enter Passcode");
+
+        while (passcode !== password) {
+            alert("Incorrect PassCode");
+            passcode = prompt("Enter Passcode"); // Ask again for passcode
+        }
+
+        // Set the passcode flag in localStorage
+        localStorage.setItem('passcodeEntered', 'true');
     }
 
-    // Make an AJAX request to retrieve the passcode
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var passcode = xhr.responseText.trim();
-                var userPasscode = prompt("Enter Passcode");
+    // Call passcodeprotect function when the page loads
+    passcodeprotect();
 
-                while (userPasscode !== passcode) {
-                    alert("Incorrect Passcode");
-                    userPasscode = prompt("Enter Passcode"); // Ask again for passcode
-                }
-
-                // Set the passcode flag in localStorage
-                localStorage.setItem('passcodeEntered', 'true');
-            } else {
-                alert("Failed to retrieve passcode. Please try again later.");
-            }
-        }
-    };
-    xhr.open('GET', 'passcode.php', true);
-    xhr.send();
-}
-
-// Call passcodeprotect function when the page loads
-passcodeprotect();
-
-alert('Welcome To Barlows Support!');
+    alert('Welcome To Barlows Support!');
