fff7c85bc23074e7b9974138cb1b3b748173de18
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-22 10:01:36 +0000

    Update contact-search.js

--- js/contact-search.js
+++ js/contact-search.js
@@ -15,3 +15,19 @@
     // Add event listener to the search input
     document.getElementById("searchInput").addEventListener("input", filterLP);
 
+// Function to filter contact list by ID
+    function filterLP(evt) {
+        let input = evt.target.value.toLowerCase();
+        let contacts = document.querySelectorAll(".main-contact");
+        contacts.forEach((contact) => {
+            let id = contact.id.toLowerCase();
+            if (!id.includes(input)) {
+                contact.classList.add("hidden"); // Add hidden class to hide the entire contact row
+            } else {
+                contact.classList.remove("hidden"); // Remove hidden class to display the contact row
+            }
+        });
+    }
+
+    // Add event listener to the search input
+    document.getElementById("searchInput").addEventListener("input", filterLP);
