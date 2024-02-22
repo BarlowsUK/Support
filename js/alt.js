171a1b1eda316002356ddaaa5f9676c7f71b6da7
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-22 10:06:51 +0000

    Update alt.js

--- js/alt.js
+++ js/alt.js
@@ -64,3 +64,36 @@ document.querySelectorAll('.filter-button').forEach(button => {
         handleFilterClick(filterValue);
     });
 });
+ // Function to filter contact list by ID
+    function filterLP(evt) {
+        let input = evt.target.value.toLowerCase();
+        let contacts = document.querySelectorAll(".contact");
+        contacts.forEach((contact) => {
+            let id = contact.id.toLowerCase();
+            if (!id.includes(input)) {
+                contact.classList.add("hidden"); // Add hidden class to hide the entire contact row
+            } else {
+                contact.classList.remove("hidden"); // Remove hidden class to display the contact row
+            }
+        });
+    }
+
+    // Add event listener to the search input
+    document.getElementById("searchInput").addEventListener("input", filterLP);
+
+// Function to filter contact list by ID
+    function filterLP(evt) {
+        let input = evt.target.value.toLowerCase();
+        let contacts = document.querySelectorAll(".main-contact");
+        contacts.forEach((contact) => {
+            let id = contact.id.toLowerCase();
+            if (!id.includes(input)) {
+                contact.classList.add("hidden"); // Add hidden class to hide the entire contact row
+            } else {
+                contact.classList.remove("hidden"); // Remove hidden class to display the contact row
+            }
+        });
+    }
+
+    // Add event listener to the search input
+    document.getElementById("searchInput").addEventListener("input", filterLP);
