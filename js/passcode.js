7566d3b85b814a5be4a8bdf2b2ba1334c6059076
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-22 11:14:00 +0000

    Update passcode.js

--- js/passcode.js
+++ js/passcode.js
@@ -1,23 +1,24 @@
-    var password = "Barlowsfeb";
+var password = "Barlowsfeb";
 
-    function passcodeprotect() {
-        // Check if the passcode flag is set in localStorage
-        if (localStorage.getItem('passcodeEntered') === 'true') {
-            return; // If passcode already entered, exit the function
-        }
-
-        var passcode = prompt("Enter Passcode");
-
-        while (passcode !== password) {
-            alert("Incorrect PassCode");
-            passcode = prompt("Enter Passcode"); // Ask again for passcode
-        }
-
-        // Set the passcode flag in localStorage
-        localStorage.setItem('passcodeEntered', 'true');
+function passcodeprotect() {
+    // Check if the passcode flag is set in localStorage
+    if (localStorage.getItem('passcodeEntered') === 'true') {
+        return; // If passcode already entered, exit the function
     }
 
-    // Call passcodeprotect function when the page loads
-    passcodeprotect();
+    var passcode = prompt("Enter Passcode");
 
+    while (passcode !== password) {
+        alert("Incorrect PassCode");
+        passcode = prompt("Enter Passcode"); // Ask again for passcode
+    }
+
+    // Set the passcode flag in localStorage
+    localStorage.setItem('passcodeEntered', 'true');
+
+    // Show the welcome message
     alert('Welcome To Barlows Support!');
+}
+
+// Call passcodeprotect function when the page loads
+passcodeprotect();
