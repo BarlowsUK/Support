3a6c2dbf5f605ffbdfc1b7424f9106b1a86276e9
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-29 11:46:07 +0000

    Update contact-search.js

--- js/contact-search.js
+++ js/contact-search.js
@@ -31,3 +31,40 @@
 
     // Add event listener to the search input
     document.getElementById("searchInput").addEventListener("input", filterLP);
+// Function to filter accordion sections by content
+function filterAccordion(evt) {
+    let input = evt.target.value.toLowerCase();
+    let accordionContents = document.querySelectorAll(".accordion-content");
+    accordionContents.forEach((content) => {
+        let sectionContent = content.textContent.toLowerCase();
+        if (!sectionContent.includes(input)) {
+            content.parentElement.classList.add("hidden"); // Hide the entire accordion section
+        } else {
+            content.parentElement.classList.remove("hidden"); // Show the accordion section
+        }
+    });
+}
+
+// Add event listener to the search input
+document.getElementById("searchbar").addEventListener("input", filterAccordion);
+
+// accordion js
+document.addEventListener("DOMContentLoaded", function() {
+  const accordionItems = document.querySelectorAll('.accordion-item');
+
+  accordionItems.forEach(item => {
+    item.addEventListener('click', function() {
+      // Close all accordion items
+      accordionItems.forEach(item => {
+        item.classList.remove('active');
+        const panel = item.querySelector('.accordion-content');
+        panel.style.display = 'none';
+      });
+
+      // Open the clicked accordion item
+      this.classList.add('active');
+      const panel = this.querySelector('.accordion-content');
+      panel.style.display = 'block';
+    });
+  });
+});
