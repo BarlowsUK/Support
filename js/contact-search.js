d4d01ee9ed2893083ac5262e99196c42aefffb43
barlowsSUPPORT <160111785+oliverBARLOWS@users.noreply.github.com>
2024-02-29 11:55:13 +0000

    Update contact-search.js

--- js/contact-search.js
+++ js/contact-search.js
@@ -31,3 +31,20 @@
 
     // Add event listener to the search input
     document.getElementById("searchInput").addEventListener("input", filterLP);
+
+// Function to filter contact list by accordion
+    function filterLP(evt) {
+        let input = evt.target.value.toLowerCase();
+        let contacts = document.querySelectorAll(".accordion-item");
+        contacts.forEach((contact) => {
+            let id = contact.id.toLowerCase();
+            if (!id.includes(input)) {
+                contact.classList.add("hidden"); // Add hidden class to hide the entire contact row
+            } else {
+                contact.classList.remove("hidden"); // Remove hidden class to display the contact row
+            }
+        });
+    }
+
+    // Add event listener to the search input
+    document.getElementById("searchInput").addEventListener("input", filterLP);
